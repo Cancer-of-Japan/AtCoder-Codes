9d4ce751806e037eee3da02f3c506842fe0dccd4
leo <leo@MacBook-Pro.local>
2021-04-20 16:37:36 +0800

    modded 117_A

--- 117_A.cpp
+++ 117_A.cpp
@@ -4,14 +4,33 @@
 
 using namespace std;
 
+
+void check_sim(int arr[],int n,int num, bool flag){
+    for(int i = 0; i < n; i++){
+        if(arr[i] == num){
+            flag = false;
+            create_num(arr[], n, num, flag)
+        }
+    }
+}
+
+void create_num(int arr[],int n,int num, bool flag){
+    
+}
+
+
+
 int main(){
     int a, b;
     cin >> a;
     cin >> b;
+    int n = a + b;
     random_device rseed;
     mt19937 rng(rseed());
     uniform_int_distribution<int> dist(-10^9, 10^9);
     int arr[a + b];
+    
+    bool flag = true;
     if(a == 1 && b == 1){
         arr[0] = 1;
         arr[1] = -1;
@@ -19,11 +38,7 @@ int main(){
     else{
         for(int i = 0; i < a + b; i++){
             int num = dist(rng);
-            for(int j = 0; j < i; j++){
-                if(arr[j] == num){
-                    num = dist(rng);
-                }
-            }
+            check_sim(arr[], i, num, flag);
         arr[i] = num;
         cout << arr[i] << "\n";
         }
