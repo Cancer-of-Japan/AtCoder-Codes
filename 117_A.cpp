4e0508847d1fe1807e3f4dd4f8c5b87250688b60
leo <leo@MacBook-Pro.local>
2021-04-20 11:31:17 +0800

    added 117 codes

---
+++ 117_A.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <time>
+#include <math>
+#include <random>
+
+
+using namespace std
+
+int main(){
+    int a, b;
+    cin >> a >> endl;
+    cin >> b >> endl;
+    int arr[a + b];
+    if(a == 1 && b == 1){
+        arr[0] = 1;
+        arr[1] = -1;
+    }
+    
+    
+}
